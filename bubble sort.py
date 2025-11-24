e8d80d70c16cc87a868e47ef2b9be12cd9a886b3
Blake2134474238784932 <bk05265122@priestley.ac.uk>
2025-11-24 10:17:01 +0000

    Add files via upload

--- bubble sort.py
+++ bubble sort.py
@@ -46,3 +46,23 @@ def while_swapped2():
                 
         n = n -1
     print(items)
+def size_list():
+    import random
+    y = 10
+    items = [random.randint(0,100) for x in range(y)]
+    print (items)
+    n = len(items)-1
+    swapped = True
+    
+    while swapped and n> 0:
+        swapped = False
+        for index in range(0, n):
+            if (items[index] > items [index + 1]):
+                temp = items[index]
+                items[index] = items[index + 1]
+                items[index + 1] = temp
+                
+                swapped = True
+                
+        n = n -1
+    print(items)
